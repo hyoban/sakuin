f54bef8654f7056efe4a2da7b083d15eec3fe048
Stephen Zhou <hi@hyoban.cc>
2024-02-11 17:42:08 +0800

    chore: clean

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-import { iconsPlugin } from "@egoist/tailwindcss-icons"
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
-  plugins: [iconsPlugin({ scale: 1.3 })],
+  plugins: [],
 }
