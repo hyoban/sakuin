89deb2ce867f8ea58110eced77bb1b58e6b9e9e6
Stephen Zhou <hi@hyoban.cc>
2024-02-11 17:39:18 +0800

    chore: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,12 @@ import "./globals.css"
 export const metadata = {
   title: "Hyoban",
   description: "Hyoban's personal website",
+  icons: [{ rel: "icon", url: "/favicon.svg" }],
 }
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
-    <html lang="zh-Hans" className="h-full">
+    <html lang="zh-Hans" className="h-full dark:bg-neutral-900 dark:text-white">
       <body className="h-full">{children}</body>
     </html>
   )
