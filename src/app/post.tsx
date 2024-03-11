594d009719fd97c1d6bfd9fb2b49818148c5d9d4
Stephen Zhou <hi@hyoban.cc>
2024-03-11 15:53:42 +0800

    feat: show interaction count

--- src/app/post.tsx
+++ src/app/post.tsx
@@ -9,7 +9,7 @@ import { unified } from 'unified'
 import { env } from '../env'
 import { getPostBySlug } from '../lib/storage'
 import { getCommentFull } from '../lib/storage/comment'
-import { Comment } from '../lib/storage/types'
+import { Comment, Interaction } from '../lib/storage/types'
 
 export async function Post({ slug }: { slug: string }) {
   const post = await getPostBySlug(env.HANDLE, slug)
@@ -26,12 +26,22 @@ export async function Post({ slug }: { slug: string }) {
       },
     })
     .use(rehypeStringify, { allowDangerousHtml: true })
-    .process(`# ${post.title}\n${post.content}`)
+    .process(post.content)
 
   const comments = await getCommentFull(env.HANDLE, post.noteId)
   return (
     <main className="mx-auto max-w-[692px] px-6 my-6 sm:my-16 antialiased prose prose-neutral dark:prose-invert">
-      <article dangerouslySetInnerHTML={{ __html: String(postContent) }} />
+      <article>
+        <h1>{post.title}</h1>
+        <Interaction
+          interaction={{
+            views: post.views,
+            likes: post.likes,
+            comments: post.comments,
+          }}
+        />
+        <div dangerouslySetInnerHTML={{ __html: postContent.toString() }} />
+      </article>
       <section>
         <h2>Comments</h2>
         <ul>
@@ -44,6 +54,25 @@ export async function Post({ slug }: { slug: string }) {
   )
 }
 
+function Interaction({ interaction }: { interaction: Interaction }) {
+  return (
+    <div className="flex gap-4">
+      <span className="flex gap-2 items-center">
+        <span className="i-lucide-eye" />
+        <span className="text-lg">{interaction.views}</span>
+      </span>
+      <span className="flex gap-2 items-center">
+        <span className="i-lucide-thumbs-up" />
+        <span className="text-lg">{interaction.likes}</span>
+      </span>
+      <span className="flex gap-2 items-center">
+        <span className="i-lucide-message-circle" />
+        <span className="text-lg">{interaction.comments}</span>
+      </span>
+    </div>
+  )
+}
+
 function Comment({ comment }: { comment: Comment }) {
   return (
     <li>
@@ -53,6 +82,14 @@ function Comment({ comment }: { comment: Comment }) {
         :
         {' '}
         {comment.content}
+        {comment.likes > 0 && (
+          <>
+            {' '}
+            <span className="i-lucide-thumbs-up text-xs align-baseline" />
+            {' '}
+            {comment.likes}
+          </>
+        )}
       </p>
       <ul>
         {comment.replies.map(reply => (
