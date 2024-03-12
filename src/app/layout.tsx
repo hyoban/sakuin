cd0a988fec1c6d47d27cdd82a8a78a3bb4cf3f7f
Stephen Zhou <hi@hyoban.cc>
2024-03-12 15:18:14 +0800

    refactor: use next font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,84 @@ import 'remark-github-alerts/styles/github-colors-dark-media.css'
 import 'remark-github-alerts/styles/github-base.css'
 import './globals.css'
 
+import localFont from 'next/font/local'
 import { getSiteInfo } from 'sakuin'
 
 import { env } from '../env'
+
+const snPro = localFont({
+  variable: '--font-fans',
+  preload: false,
+  src: [
+    {
+      path: '../font/SNPro-Thin.otf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../font/SNPro-ThinItalic.otf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../font/SNPro-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../font/SNPro-LightItalic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../font/SNPro-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../font/SNPro-RegularItalic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../font/SNPro-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../font/SNPro-MediumItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../font/SNPro-Semibold.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../font/SNPro-SemiboldItalic.otf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../font/SNPro-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../font/SNPro-BoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+})
 
 export default async function RootLayout({ children }: React.PropsWithChildren) {
   const { HANDLE, SITE_URL } = env
   const { siteName, characterName, description, icon, banner } = await getSiteInfo(HANDLE)
 
   return (
-    <html lang="en" className="dark:bg-neutral-900 dark:text-white">
+    <html lang="en" className={`dark:bg-neutral-900 dark:text-white ${snPro.variable}`}>
       <title>{siteName ?? characterName}</title>
       <meta name="description" content={description} />
       {icon && <link rel="icon" type="image/png" href={icon} />}
