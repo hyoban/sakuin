2cc802cd23a687eb90456cf2a118e6ae0363b0cb
Stephen Zhou <hi@hyoban.cc>
2024-03-13 18:50:16 +0800

    feat: real pagination

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -6,7 +6,7 @@ import { PostList } from './post-list'
 export const revalidate = 3600
 
 export default async function HomePage() {
-  const posts = await client.post.getAll(env.HANDLE, { orderBy: 'publishedAt' })
+  const { list: posts, cursor } = await client.post.getMany(env.HANDLE, { orderBy: 'publishedAt' })
   const postsWithCoverSize = await Promise.all(posts.map(async (post) => {
     if (!post.cover)
       return { ...post, coverSize: null }
@@ -19,6 +19,9 @@ export default async function HomePage() {
   }))
 
   return (
-    <PostList posts={postsWithCoverSize} />
+    <PostList
+      posts={postsWithCoverSize}
+      cursor={cursor}
+    />
   )
 }
--- src/app/(homepage)/post-list.tsx
+++ src/app/(homepage)/post-list.tsx
@@ -1,42 +1,43 @@
 'use client'
-
-import { useState } from 'react'
+import { useState, useTransition } from 'react'
 import type { Post } from 'sakuin'
 
+import { fetchMorePost } from './action'
 import { PostItem } from './post-item'
 
-const pageSize = 8
-
-export function PostList({ posts }: { posts: Array<Post & { coverSize: { width: number, height: number } | null }> }) {
-  const [page, setPage] = useState(1)
-  const maxPage = Math.ceil(posts.length / pageSize)
+export function PostList({
+  posts,
+  cursor,
+}: {
+  posts: Array<Post & { coverSize: { width: number, height: number } | null }>
+  cursor: string | null
+}) {
+  const [currentPosts, setCurrentPosts] = useState(posts)
+  const [currentCursor, setCurrentCursor] = useState(cursor)
+  const [isLoadMorePending, startLoadMoreTransition] = useTransition()
 
   return (
     <>
-      {posts
-        .slice((page - 1) * pageSize, page * pageSize)
-        .map(post => (
-          <PostItem key={post.slug} post={post} />
-        ))}
-      {posts.length > pageSize && (
-        <div className="flex justify-center my-6">
-          <button
-            type="button"
-            onClick={() => { setPage(page - 1) }}
-            disabled={page === 1}
-            className="px-2 py-1 rounded-full mr-4 hover:bg-neutral-100 disabled:hover:bg-inherit dark:hover:bg-neutral-800 opacity-60 hover:opacity-100 disabled:opacity-20"
-          >
-            Previous
-          </button>
-          <button
-            type="button"
-            onClick={() => { setPage(page + 1) }}
-            disabled={page === maxPage}
-            className="px-2 py-1 rounded-full hover:bg-neutral-100 disabled:hover:bg-inherit dark:hover:bg-neutral-800 opacity-60 hover:opacity-100 disabled:opacity-20"
-          >
-            Next
-          </button>
-        </div>
+      {currentPosts.map(post => <PostItem key={post.slug} post={post} />)}
+      {currentCursor && (
+        <button
+          type="button"
+          onClick={() => {
+            startLoadMoreTransition(
+              async () => {
+                const { posts, cursor } = await fetchMorePost(currentCursor)
+                setCurrentPosts([...currentPosts, ...posts])
+                setCurrentCursor(cursor)
+              },
+            )
+          }}
+          disabled={isLoadMorePending}
+          className="px-2 py-1 rounded-full hover:bg-neutral-100 disabled:hover:bg-inherit dark:hover:bg-neutral-800 opacity-60 hover:opacity-100 disabled:opacity-20"
+        >
+          {isLoadMorePending
+            ? <span className="ml-2 i-lucide-loader-2 animate-spin" />
+            : <span>Next</span>}
+        </button>
       )}
     </>
   )
