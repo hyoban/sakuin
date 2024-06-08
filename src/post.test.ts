c5d34377c27e38012c0c36759d54c673929eabb5
Stephen Zhou <hi@hyoban.cc>
2024-06-08 21:57:02 +0800

    fix: summary api

--- src/post.test.ts
+++ src/post.test.ts
@@ -5,8 +5,8 @@ import { Client } from '.'
 const client = new Client()
 
 test('get post list', async () => {
-  const { list: postList } = await client.post.getMany('diygod', { limit: 2 })
-  expect(postList).toHaveLength(2)
+  const { list: postList } = await client.post.getMany('diygod', { limit: 3 })
+  expect(postList).toHaveLength(3)
   for (const key of [
     'noteId',
     'title',
