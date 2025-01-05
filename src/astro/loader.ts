d097febbf705c0d23ffcc890f2100142618880a1
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-01-05 10:40:13 +0800

    feat: astro loader

---
+++ src/astro/loader.ts
@@ -0,0 +1,56 @@
+import { createMarkdownProcessor } from '@astrojs/markdown-remark'
+import type { Loader } from 'astro/loaders'
+import { z } from 'astro/zod'
+
+import type { ClientOptions } from '../index'
+import { Client } from '../index'
+
+export function postLoader(
+  {
+    handle,
+    ...options
+  }: ClientOptions & {
+    handle: string
+  },
+): Loader {
+  const client = new Client(options)
+
+  return {
+    name: 'xlog-post-loader',
+    async load({ config, store }) {
+      const markdownProcessor = await createMarkdownProcessor(config.markdown)
+      const posts = (await client.post.getAll(handle))
+        .sort((a, b) => {
+          return new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+        })
+
+      for (const post of posts) {
+        const { code: html } = await markdownProcessor.render(post.content)
+
+        store.set({
+          id: post.slug,
+          data: {
+            slug: post.slug,
+            title: post.title,
+            description: post.summary,
+            summary: post.summary,
+            tags: post.tags,
+            cover: post.cover,
+            date: new Date(post.publishedAt),
+          },
+          body: post.content,
+          rendered: { html },
+        })
+      }
+    },
+    schema: z.object({
+      slug: z.string(),
+      title: z.string(),
+      description: z.string(),
+      summary: z.string(),
+      tags: z.array(z.string()),
+      cover: z.string(),
+      date: z.coerce.date(),
+    }),
+  }
+}
